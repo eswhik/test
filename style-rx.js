b6c174f9a5a2fa0614b40b3c6d7bc0d78bb180d3
José <159473753+eswhik@users.noreply.github.com>
2024-07-21 00:56:05 -0500

    Create style-rx.js

---
+++ style-rx.js
@@ -0,0 +1,58 @@
+.rcgd {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-code-roblox {
+    background-color: #121b20;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.226);
+    border-bottom: solid 4px #00ffad;
+    margin: 20px 0;
+    padding: 15px;
+    max-width: 400px;
+    width: 100%;
+    border-radius: 8px;
+}
+
+.card-header-roblox {
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 10px;
+}
+
+.codigo-roblox {
+    color: #f9f9f9;
+    background: radial-gradient(circle, rgb(2, 129, 98) 0%, rgb(26 35 31) 100%);
+    border: solid 2px #00ffad;
+    text-shadow: 2px 2px 4px #037f57;
+    margin: 10px 0;
+    padding: 10px;
+    border-radius: 5px;
+}
+
+#timer-roblox {
+    background: #43525a54;
+    border: solid 1px #43525a;
+    color: #f9f9f9;
+    text-align: center;
+    padding: 5px;
+    font-size: 18px;
+    margin-top: 10px;
+    border-radius: 5px;
+}
+
+.flg-roblox {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.logo-roblox {
+    width: 200px;
+    height: 50px;
+    background-image: url('https://images.rbxcdn.com/d82801b936c26e174c4e782510a72d78-roblox_logo_dark_08292022.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+}
